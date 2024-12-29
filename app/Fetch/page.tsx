bb4bf0b540e75de7822d6c57072e31b328717392
ubaidalam01 <160783629+ubaidalam01@users.noreply.github.com>
2024-12-30 03:23:04 +0500

    Update Fetch

--- app/Fetch/page.tsx
+++ app/Fetch/page.tsx
@@ -66,7 +66,7 @@ const Fetch = () => {
           of creativity and community.
         </p>
 
-        <h2 className="text-2xl font-bold p-4 mb-4">A Lineup to Remember</h2>
+        <h2 className="text-2xl font-bold p-4 mb-4 text-black">A Lineup to Remember</h2>
         <p className="text-base text-gray-700 mb-4 border-l-4 border-black p-4 font-serif">
           &quot;Who&apos;s performing this year?&quot; From globally renowned headliners to
           emerging local talents, FETCH Festival BRLN 2023 offers a diverse
@@ -75,7 +75,7 @@ const Fetch = () => {
           dancing from day to night.
         </p>
 
-        <h2 className="text-2xl font-bold mb-4 p-4">
+        <h2 className="text-2xl font-bold mb-4 p-4 text-black">
           Art Installations and Workshops
         </h2>
         <p className="text-base text-gray-700 p-4 mb-4 font-serif">
@@ -86,7 +86,7 @@ const Fetch = () => {
           festival-goers.
         </p>
 
-        <h2 className="text-2xl font-bold mb-4 p-4">Unite and Celebrate</h2>
+        <h2 className="text-2xl font-bold mb-4 p-4 text-black">Unite and Celebrate</h2>
         <p className="text-3xl text-gray-700 p-4 border-l-4 border-black font-serif">
           &ldquo;Join the movement and feel the rhythm of Berlin.&rdquo;
         </p>
@@ -101,7 +101,7 @@ const Fetch = () => {
 
       {/* Comment Section */}
       <div className="w-full max-w-3xl mt-10 bg-white p-4">
-        <h3 className="text-2xl font-bold mb-4">Comments</h3>
+        <h3 className="text-2xl font-bold mb-4 text-black">Comments</h3>
 
         {/* Input for new comment */}
         <textarea
@@ -112,7 +112,7 @@ const Fetch = () => {
         ></textarea>
         <button
           onClick={addComment}
-          className="px-9 border-black border-2 py-2 hover:bg-black hover:text-white transition duration-300 ease-in-out"
+          className="px-9 border-black border-2 py-2 text-black hover:bg-black hover:text-white transition duration-300 ease-in-out"
         >
           Add Comment
         </button>
